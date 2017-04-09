837040c1332729f55c1fa2054f568a2e63e31395
Brendan Rogers <brendan@utexas.edu>
2017-04-09 15:27:22 -0500

    added basic files and keys and tokens

---
+++ assets/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'zurxNo3m2qqqBs9hiLu38E9pi',
+  consumer_secret: 'BAoqbwHrZmKBiQkXijlwhdxyNwrB23Xl3iVXCACx1rR69oQmEi',
+  access_token_key: '851139211575119875-JsVNRNLyvqhJIW6aKpwLXmwFbPb6QDR',
+  access_token_secret: '3IMO3kvxGEJccYk3Iq5RcN4quFMCXKcVKHnRht0j7nbTj',
+}
