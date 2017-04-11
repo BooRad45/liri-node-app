d9ec4dd28f27d11622d23c617a0924289a3aee7e
Brendan Rogers <brendan@utexas.edu>
2017-04-10 20:56:06 -0500

    Drilled down to artist name

--- liri.js
+++ liri.js
@@ -8,32 +8,39 @@ var fs = require("fs");
 // console.log("Please enter a command for LiriBot: my-tweets <username>, spotify-this-song <song name>, movie-this <movie name >, or do-what-it-says");
 
 // Create a "Prompt" with a series of questions.
-var nodeArgs = process.argv;
 var command = process.argv[2];
-
-
  
  	if (command === "my-tweets") {
-	var screenName = process.argv[3];
 
-
-	client.get('statuses/user_timeline', { user_id: screenName, count: 20, trim_user: true }, function(error, tweets, response) {
+	client.get('statuses/user_timeline', { user_id: "BooRad45", count: 20, trim_user: true }, function(error, tweets, response) {
 		if (!error) {
 			for (var i = 0; i < tweets.length; i++) {
 
-
     console.log(tweets[i].created_at + tweets[i].text);
-    }
-  }
-});
-		
-		
-  		
-
+    		}
+  		}
+	});
   }
 
-// else if {
-// 	(command === "spotify-this-song") { 
+  else if (command === "spotify-this-song") {
+  	console.log(command);
+  	var song = process.argv[3];
+
+  	spotify.search({ type: 'track', query: song }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    		} 
+    		else {
+    			//get just artist info for track
+    			var dataArray = data.tracks.items.length;
+    			console.log(dataArray);
+    			for (var i = 0; i < dataArray; i++) {
+    			console.log(data.tracks.items[i].artists[0].name);
+    		  }
+    		}
+	  });
+  }
 	
 
 
